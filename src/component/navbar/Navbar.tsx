b2bca3cbc99e99b1be82e9d976a5edbc1722fdfb
RohanKumarMainali <rohanmainali39@gmail.com>
2022-12-05 21:23:17 +0545

    font-awesome

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -3,26 +3,30 @@ import './Navbar.css';
 let logo = require('../../images/sadhanLogo.png')
 
 function Navbar() {
-  return (
-    <div>
-        <div className="container">
-            <div className="row">
-                <div className="col-md-3 navbar-logo">
-                    <img src={logo} alt="Logo" />
-                </div>
-                <div className="col-md-6">
-                    Search
-                </div>
-                <div className="col-md-3">
-                    <nav>
-                    nav
-                    </nav>
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-3 navbar-logo">
+                        <img src={logo} alt="Logo" />
+                    </div>
+                    <div className="col-md-6">
+                        <form action="">
+                            <input type="search" required />
+                            <i className="fa fa-search"></i>
+                            <a href="hello" id="clear-btn">Clear</a>
+                        </form>
+                    </div>
+                    <div className="col-md-3">
+                        <nav>
+                            nav
+                        </nav>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-     
-  )
+
+    )
 }
 
 export default Navbar

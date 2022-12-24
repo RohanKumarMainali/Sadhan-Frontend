040064864934b0cb73776fd213b3c0acfd662f62
984345 <rohanmainali39@gmail.com>
2022-12-24 22:49:27 +0545

    chakra ui modal

--- src/component/banner/Banner.tsx
+++ src/component/banner/Banner.tsx
@@ -1,15 +1,71 @@
 import React from 'react'
 import './Banner.css'
 import Navbar from '../navbar/Navbar';
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,FormControl,
+  FormLabel,
+  Input,
+
+} from '@chakra-ui/react'
+
 //test the ssh
 let bikes = require('../../images/bikes.jpg');
 let bike = require('../../images/bike.jpg');
 let yellowBike = require('../../images/yellowBike.png');
 
 function Banner() {
+
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const initialRef = React.useRef(null)
+  const finalRef = React.useRef(null)
+
     return (
         <div>
+     
+<Button onClick={onOpen}>Open Modal</Button>
+      <Button ml={4} ref={finalRef}>
+        I'll receive focus on close
+      </Button>
+     <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create your account</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>First name</FormLabel>
+              <Input ref={initialRef} placeholder='First name' />
+            </FormControl>
 
+            <FormControl mt={4}>
+              <FormLabel>Last name</FormLabel>
+              <Input placeholder='Last name' />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
             <div className="container-fluid ">
                     <div className="row align-items-center"  >
                             <div className="bike-image"

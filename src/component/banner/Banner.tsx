6ecadfcf6e65abde8aaded607222862e2cdb62e0
984345 <rohanmainali39@gmail.com>
2022-12-24 21:50:30 +0545

    banner image

--- src/component/banner/Banner.tsx
+++ src/component/banner/Banner.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Banner.css'
+import Navbar from '../navbar/Navbar';
 
 let bikes = require('../../images/bikes.jpg');
 let bike = require('../../images/bike.jpg');
@@ -10,42 +11,24 @@ function Banner() {
         <div>
 
             <div className="container-fluid ">
-                {/* <div className="banner-img"
-                    style={{
-                        background: 'linear-gradient(0deg,transparent 345px, #fff 0 0 ) bottom right',
-                        width: '100%',
-                        height: ' 700px',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundSize: 'cover',
-                        // backgroundPosition: 'center'
-                    }}
-                > */}
                     <div className="row align-items-center"  >
-                        <div className="col-md-5" >
-                            <div className="banner-text">
-                                <h3>
-                                    High Service<br></br>
-                                    for every<br></br>
-                                    customer
-                                </h3>
-                            </div>
-                        </div>
-                        <div className="col-md-7 p-0 banner-img" >
                             <div className="bike-image"
-
                                 style={{
                                     backgroundImage: `url(${bikes})`,
                                     maxWidth: '100%',
-                                    height: '600px',
-                                    
+                                    height: '100vh',
                                     backgroundRepeat: 'no-repeat',
                                     backgroundSize: 'cover',
                                     backgroundPosition: 'top',
 
                                 }}     >
                                     
-                            </div>
                         </div>
+                            <div className='banner-text1'>
+                                <h1>Find Your Drive</h1>
+                                <h5>Explore the world's largest vehicle sharing marketplace</h5>
+                            </div>
+
 
 
 

8fae91b3e2eb465a6c854b4b3e8b8f45b57b3745
984345 <rohanmainali39@gmail.com>
2022-12-27 16:24:38 +0545

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
@@ -6,58 +6,59 @@ import Navbar from './component/navbar/Navbar'
 import axios from 'axios'
 
 
-interface userInfo{
+interface userInfo {
 
     name: string,
     isLoggedIn: boolean,
-    }
+}
 
 function App() {
-    
-  const[user,setUser] = useState({});
-  const getUser = () => {
- axios
-  .get("http://localhost:5000/api/login/success",{
-       withCredentials: true 
-    },)
-  .then(data => console.log(data))
-  .catch(error => console.log('errror ' + error));
-  }; 
-  const getUsers = () => {
-      fetch("http://localhost:5000/api/login/success", {
-        method: "GET",
-        credentials: "include",
-        },
-      )
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
-        })
-        .then((resObject) => {
-          setUser(resObject.user);
-          console.log(resObject.user);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+
+    const [user, setUser] = useState({});
+    const getUser = () => {
+        axios
+            .get("http://localhost:5000/api/login/success", {
+                withCredentials: true
+            },)
+            .then(data => console.log(data))
+            .catch(error => console.log('errror ' + error));
     };
-useEffect(() => {
-    getUsers();
-  },[]);
+    const getUsers = () => {
+        fetch("http://localhost:5000/api/login/success", {
+            method: "GET",
+            credentials: "include",
+        },
+        )
+            .then((response) => {
+                if (response.status === 200) return response.json();
+                throw new Error("authentication has been failed!");
+            })
+            .then((resObject) => {
+                setUser(resObject.user);
+                console.log(resObject.user);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    useEffect(() => {
+        getUsers();
+    }, []);
 
     const obj = {
-        isLoggedIn:true} 
-  return (
-    <div className="App">
-    <Navbar user= {user}/>
-      <Router>
-        <Routes>
-          <Route  path ='/' element = {<Home/>}/>
-        </Routes>
-      </Router>
-    </div>
+        isLoggedIn: true
+    }
+    return (
+        <div className="App">
+            <Navbar user={user} />
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </Router>
+        </div>
 
-  );
+    );
 }
 
 export default App;

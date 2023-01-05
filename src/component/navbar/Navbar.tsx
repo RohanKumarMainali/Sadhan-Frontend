deb3ea83da93181cdcaa861f03d46eb05355aef4
984345 <rohanmainali39@gmail.com>
2023-01-05 11:47:57 +0545

    props sent to modal

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -5,51 +5,15 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { useRef } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
-import AlertPop from '../modal//Alert';
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    Button, FormControl,
-    FormLabel,
-    Input,
-
-} from '@chakra-ui/react'
-
-import { FcGoogle } from 'react-icons/fc';
-import { BsFacebook, BsTwitter } from 'react-icons/bs';
+import LoginModal from '../modal/LoginModal'
 let logo = require('../../images/newLogo.png')
 
 function Navbar({ user }: any) {
     const search = useRef<HTMLInputElement>(null);
-    const initialValue = {
-        email: "",
-        password: "",
-        firstName: "",
-        lastName: "",
-        signupEmail: "",
-        signupPassword: "",
-    }
-
-    const [values, setValues] = useState(initialValue);
     const [statusCode, setStatusCode] = useState(0);
+    const [showLogin, setShowLogin] = useState(false);
 
-    const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-        // name-> e.target.name
-        // value-> e.target.value
-        const { name, value } = e.currentTarget;
-
-        setValues({
-            ...values,
-            [name]: value
-        })
-    }
-    const url = 'http://localhost:5000/api';
+   const url = 'http://localhost:5000/api';
 
     const storeAuthentication = (user: any) => {
         localStorage.setItem("user", JSON.stringify(user));
@@ -69,35 +33,9 @@ function Navbar({ user }: any) {
         }
     }
 
-    const login = async () => {
-        try {
-            const payload = { email: values.email, password: values.password }
-            const response = await axios.post(`${url}/user/login`, payload, {
-                withCredentials: true,
-                headers: {
-                    'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'
-                }
-            });
-            if (response.status == 200) {storeAuthentication(response.data);
-                setStatusCode(200);
-                onLoginClose();
-
-            }
-            console.log(JSON.stringify(response.data.message))
-
-        } catch (error: any) {
-            let status = error.response.status;
-            if (status == 401) setStatusCode(401);
-        }
-    }
-    const focusSearch = () => {
+   const focusSearch = () => {
         search.current?.focus();
-        console.log(values)
     }
-    const { isOpen: isSignupOpen, onOpen: onSignupOpen, onClose: onSignupClose } = useDisclosure()
-    const { isOpen: isLoginOpen, onOpen: onLoginOpen, onClose: onLoginClose } = useDisclosure()
-    const initialRef = React.useRef(null)
-    const finalRef = React.useRef(null)
 
     const google = () => {
         window.open('http://localhost:5000/api/google', '_self');
@@ -115,130 +53,6 @@ function Navbar({ user }: any) {
         <div>
             <nav className='main-navbar'>
 
-
-                <Modal
-                    initialFocusRef={initialRef}
-                    finalFocusRef={finalRef}
-                    isOpen={isLoginOpen}
-                    onClose={onLoginClose}
-                >
-                    <ModalOverlay />
-                    <ModalContent>
-                        <ModalHeader>Login</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody pb={10} className='login-body'>
-                            <FormControl>
-                                <FormLabel>Email</FormLabel>
-                                <Input name='email' value={values.email} onChange={handleInputChange} ref={initialRef} placeholder='Email' />
-                            </FormControl>
-
-                            <FormControl mt={6}>
-                                <FormLabel>Password</FormLabel>
-                                <Input name='password' type='password' value={values.password} onChange={handleInputChange} placeholder='Password' />
-                            </FormControl>
-
-                            <button className='login-btn' onClick={login}>
-                                Login
-                            </button>
-                            {statusCode == 401 ? <div>
-                                <AlertPop statusCode={401} message='Incorrect username or password ' />
-                            </div> : <></>}
-                            <FormControl mt={6}>
-                                <button className='social-login-btn' onClick={google}>
-                                    <FcGoogle className='social-logo' /> Continue with Google
-                                </button>
-                            </FormControl>
-
-                            <FormControl mt={3}>
-                                <button className='social-login-btn'>
-                                    <BsFacebook color='3b5998' className=' social-logo' /> Continue with Facebook
-                                </button>
-                            </FormControl>
-
-                            <FormControl mt={3}>
-                                <button className='social-login-btn'>
-                                    <BsTwitter color='00acee' className='social-logo' /> Continue with Twitter
-                                </button>
-                            </FormControl>
-
-                            <div className='login-footer'>
-                                <p>Don't have an account?</p>
-                                <button onClick={() => { onLoginClose(); onSignupOpen(); }} className='signup-btn'>
-                                    Sign up
-                                </button>
-                            </div>
-
-                        </ModalBody>
-
-                    </ModalContent>
-                </Modal>
-
-                <Modal
-                    initialFocusRef={initialRef}
-                    finalFocusRef={finalRef}
-                    isOpen={isSignupOpen}
-                    onClose={onSignupClose}
-                >
-                    <ModalOverlay />
-                    <ModalContent>
-                        <ModalHeader>Let's Get Started</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody pb={10} className='signup-body'>
-                            <div className='signup-name'>
-                                <FormControl style={{ width: '43%' }}>
-                                    <FormLabel>First Name</FormLabel>
-                                    <Input name='firstName' value={values.firstName} onChange={handleInputChange} ref={initialRef} placeholder='First Name' />
-                                </FormControl>
-                                <FormControl style={{ width: '43%' }}>
-                                    <FormLabel>Last Name</FormLabel>
-                                    <Input name='lastName' value={values.lastName} onChange={handleInputChange} placeholder='Last Name' />
-                                </FormControl>
-                            </div>
-
-                            <FormControl mt={2}>
-                                <FormLabel>Email</FormLabel>
-                                <Input name='signupEmail' value={values.signupEmail} onChange={handleInputChange} placeholder='Email' />
-                            </FormControl>
-
-                            <FormControl mt={2}>
-                                <FormLabel>Password</FormLabel>
-                                <Input name='singupPassword' value={values.signupPassword} onChange={handleInputChange} placeholder='Password' />
-                            </FormControl>
-
-
-                            <button className='login-btn' >
-                                Signup
-                            </button>
-                            <FormControl mt={6}>
-                                <button className='social-login-btn'>
-                                    <FcGoogle className='social-logo' /> Continue with Google
-                                </button>
-                            </FormControl>
-
-                            <FormControl mt={3}>
-                                <button className='social-login-btn'>
-                                    <BsFacebook color='3b5998' className=' social-logo' /> Continue with Facebook
-                                </button>
-                            </FormControl>
-
-                            <FormControl mt={3}>
-                                <button className='social-login-btn'>
-                                    <BsTwitter color='00acee' className='social-logo' /> Continue with Twitter
-                                </button>
-                            </FormControl>
-
-                            <div className='login-footer'>
-                                <p>Already have an account?</p>
-                                <button className='signup-btn' onClick={() => { onSignupClose(); onLoginOpen() }}>
-                                    Login
-                                </button>
-                            </div>
-
-                        </ModalBody>
-
-                    </ModalContent>
-                </Modal>
-
                 <div className="container">
                     <div className="row">
                         <div className="col-md-3 navbar-logo">
@@ -254,7 +68,6 @@ function Navbar({ user }: any) {
                         </div>
                        <div className="col-md-5 nav-container">
                             <nav>
-
                                 <ul>
                                     <button className='btn'>Rent Vehicle</button>
                                     <button className='btn'>Become Host</button>
@@ -265,10 +78,12 @@ function Navbar({ user }: any) {
                                             <button className='btn btn-primary' onClick={logout} >Logout</button>
                                         </li>
                                     </>) :
-                                        (<button className='btn btn-primary' onClick={onLoginOpen}>Login</button>)
+                                        (<button className='btn btn-primary' onClick = {()=> setShowLogin(true)} >Login</button>)
                                     }
                                 </ul>
                             </nav>
+                        
+                        <LoginModal show = {showLogin} close ={()=>setShowLogin(!showLogin)}/>
                         </div>
 
                     </div>

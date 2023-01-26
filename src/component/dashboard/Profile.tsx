12e7427fec457ec5e4162f0371ee669e06ba04b3
984345 <rohanmainali39@gmail.com>
2023-01-26 14:02:08 +0545

    profile

--- src/component/dashboard/Profile.tsx
+++ src/component/dashboard/Profile.tsx
@@ -1,96 +1,91 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import {AiOutlineStar} from 'react-icons/ai';
 const Profile = () => {
 
 
     return (
-        <div className=' w-[calc(100%-14rem)]  float-right'>
-            <div className='main-profile w-4/5 mt-14 mx-auto '>
-                <div className='row'>
-                    <div className='col-8 h-50 bg-slate-50 '>
-                        <form className="w-full">
-                            <h1 className="text-gray-800 text-2xl text-left py-2">Edit profile</h1>
-                            <div className="form-input-section">
-                                <div className="flex user-info-1 mt-2 mb-2">
-                                    <div className="user-info-input p-1 w-1/4 flex flex-col align-center">
-                                        <label className="text-left text-gray-700">FIRST NAME</label>
-                                        <input type='text' className="w-full px-3  py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-400 shadow-sm" placeholder="First Name"></input>
-                                    </div>
-                                    <div className="user-info-input flex p-1 w-1/4 flex-col align-center">
-                                        <label className="text-left text-gray-700">LAST NAME</label>
-                                        <input type='text' className="w-full px-3  py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-400 shadow-sm" placeholder="Last Name"></input>
-                                    </div>
-                                    <div className="user-info-input p-1 flex flex-col w-2/4 align-center">
-                                        <label className="text-left text-gray-700">EMAIL</label>
-                                        <input type='email' className="w-full px-3  py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-400 shadow-sm" placeholder="Email"></input>
-                                    </div>
+        <div className=" w-[calc(100%-14rem)]  float-right h-screen bg-red bg-slate-100">
+            <div className="dashboard-home flex bg-white main-profile w-4/5 mt-14 mx-auto  rounded shadow-xl" style={{ height: '90vh' }}>
 
+                <div className=' mx-auto w-full'>
+                    <div className=' h-1/5 bg-gradient-to-r from-slate-50 to-blue-100'></div>
+                    <div className=' w-11/12 p-0  h-4/5 mx-auto'>
 
-
+                        <div className='h-3/5 main '>
+                            <div className='profile flex w-2/5 space-x-4 items-center'>
+                                <div className="h-40 float-left  w-40 -mt-12 border-4 border-gray-400 profile-img align-center bg-no-repeat bg-center bg-cover rounded-full" style={{ backgroundImage: `url('https://media.istockphoto.com/id/1040308104/photo/mature-handsome-business-man.jpg?s=612x612&w=0&k=20&c=QbyH3XFmLOoy8NESjLQC8PYsm6g3UBL6COFaF-Qnnbk=')` }}>
+                                </div>
+                                <div>
+                                    <h1 className=" text-sm   text-left text-gray-700 text-2xl font-bold ">Rohan Kumar Mainali</h1>
+                                
+                                       <p className='text-sm text-left'> Joined : 2012/12</p>
                                 </div>
 
-                                <div className="flex user-info-2 mt-2 mb-2">
-                                    <div className="user-info-input p-1 w-full flex flex-col align-center">
-                                        <label className="text-left text-gray-700"> ADDRESS</label>
-                                        <input type='text' name="address" className="w-full px-3  py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-400 shadow-sm" placeholder="Address"></input>
-                                    </div>
-
-
-
-                                </div>
-                                <div className="flex user-info-3 mt-2 mb-2">
-                                    <div className="user-info-input p-1 w-1/4 flex flex-col align-center">
-                                        <label className="text-left text-gray-700">COUNTRY</label>
-                                        <input type='text' className="w-full px-3  py-2  border-gray-300 rounded-lg focus:outline-none focus:border-indigo-300 shadow-sm" placeholder="Country"></input>
-                                    </div>
-                                    <div className="user-info-input flex p-1 w-1/4 flex-col align-center">
-                                        <label className="text-left text-gray-700">CITY</label>
-                                        <input type='text' className="w-full px-3  py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-400 shadow-sm" placeholder="City"></input>
-                                    </div>
-                                    <div className="user-info-input p-1 flex flex-col w-2/4 align-center">
-                                        <label className="text-left text-gray-700">POSTAL CODE</label>
-                                        <input type='email' className="w-full px-3  py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-400 shadow-sm" placeholder="Zip Code"></input>
-                                    </div>
-                                </div>
-
-                                <div className="flex user-info-4 mt-2 mb-2">
-                                    <div className="user-info-input p-1 w-full flex flex-col align-center">
-                                        <label className="text-left text-gray-700">ABOUT</label>
-                                        <textarea className="w-full px-3  py-2  border-gray-300 rounded-lg focus:outline-none focus:border-indigo-300 shadow-sm"
-                                            placeholder="Country"></textarea>
-                                    </div>
-                                </div>
-
-                                <div className="flex user-info-4 mt-2 mb-2 float-right">
-                                    <div className="user-info-input p-1 w-full flex flex-col align-right ">
-                                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update</button>
-                                    </div>
-                                </div>
                             </div>
-                        </form>
-                    </div>
-                    <div className='col-4 '>
-                        <div className='profile card-item h-3/4 w-3/4 p-0 align-center mx-auto '>
-                            <div className='card-image h-2/5 overflow-hidden bg-no-repeat bg-center bg-cover' style={{ backgroundImage: `url('https://demos.creative-tim.com/light-bootstrap-dashboard-react/static/media/photo-1431578500526-4d9613015464.0c528dc7.jpeg')` }}>
-                                <img className='w-full' src=''></img>
-                            </div>
-                            <div className='card-body h-3/5'>
-                                <div className="h-28 w-28 -mt-12 border-4 border-gray-400 mx-auto profile-img align-center bg-no-repeat bg-center bg-cover rounded-full" style={{ backgroundImage: `url('https://media.istockphoto.com/id/1040308104/photo/mature-handsome-business-man.jpg?s=612x612&w=0&k=20&c=QbyH3XFmLOoy8NESjLQC8PYsm6g3UBL6COFaF-Qnnbk=')` }}>
-                                </div>
-                                <div className="card-body-content">
-                                    <p className="profile-card-name text-sm mt-2 text-indigo-500 text-center">Rohan Kumar Mainali</p>
-                                    <p className="about-card text-sm mt-2 text-gray-700 text-center w-3/4 align-center mx-auto">I am an enthusiastic, self-motivated, reliable, responsible and hard working person. I am a mature team worker and adaptable to all challenging situations. </p>
+                            <div className='details w-3/4 mx-auto'>
+                                <div className='sub-details flex float-left  space-x-24 justify-center '>
+                                <div className= 'left-info'>
+                                    <div className='basic-info flex p-3 '>
+                                       <div className= 'verify-left '>
+
+                                        <p className='text-left text-lg'>Email: </p>
+                                        <p className='text-left text-lg'>Role: </p>
+                                        <p className='text-left text-lg'>User verification: </p>
+                                        <p className='text-left text-lg'>Approve to drive ?</p>
+                                        <p className='text-left text-lg'>Email address </p>
+                                        <p className='text-left text-lg'>Phone number</p>
+
+                                        </div>
+                                       <div className= 'verify-right '>
+                                        <p className='text-right text-lg '>rohanmainali@gmail.com</p>
+                                        <p className='text-right text-lg '>User </p>
+                                        <p className='text-right text-lg text-red-400'>Pending </p>
+
+                                        <p className='text-right text-lg text-indigo-500'>Verify license</p>
+                                        <p className='text-right text-lg text-indigo-500'>Verify Email address </p>
+                                        <p className='text-right text-indigo-500 '>Verify Number</p>
+
+                                        </div>
+
+                                            
+                                            </div>
+
+                        <Link to="/dashboard/change-password"><button className="w-3/4 mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">Change Password</button></Link>
+                                            </div>
+
+
+                                    <div className='reviews'>
+                                      <h1  className='text-2xl  text-left font-semibold text-gray-700' > Reviews</h1>
+                                      <div className= 'review section'>
+                                          <div className = 'stars flex mt-2'>
+                                                <li className='list-none'><AiOutlineStar/></li>
+                                                <li className='list-none'><AiOutlineStar/></li>
+                                                <li className='list-none'><AiOutlineStar/></li>
+                                                <li className='list-none'><AiOutlineStar/></li>
+                                                <li className='list-none'><AiOutlineStar/></li>
+                                          </div>
+                                          <div className= 'review-details'>
+                                             <span><h2 className = 'text-sm text-gray-700 font-semibold'>No reviews yet</h2></span>
+
+                                          </div>
+                                      </div>
+                                    </div>
                                 </div>
                             </div>
 
                         </div>
 
-
-                        <Link to = "/dashboard/change-password"><button className="w-3/4 mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ">Change Password</button></Link>
-
+                
+ 
                     </div>
+
+
                 </div>
+
             </div>
         </div>
+
+
     )
 }
 

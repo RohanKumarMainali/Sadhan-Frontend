1f9852a6feac6880ae4ce4a227797f4ad8b75d14
RohanKumarMainali <rohanmainali39@gmail.com>
2023-02-22 22:45:56 +0545

    auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 
+import {useAppDispatch, useAppSelector} from '../app/hooks'
+import {loginAuth} from '../features/login/loginSlice'
+
 export function useAuth() {
     const [user, setUser] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(true);
+    const login = useAppSelector((state)=>state.login.loggedIn)
+    const dispatchRedux = useAppDispatch();
 
+    function finalLogin(){ dispatchRedux(loginAuth());}
     useEffect(() => {
 
         const user = localStorage.getItem('user');
+
+ 
         if (user) {
             try {
                 setUser(JSON.parse(user));

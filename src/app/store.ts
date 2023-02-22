b7ebbc75d8596b23b04dbd4f7a783c2b8233973c
RohanKumarMainali <rohanmainali39@gmail.com>
2023-02-22 22:45:09 +0545

    store redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/CounterSlice'
+import loginReducer from '../features/login/loginSlice'
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        login: loginReducer
     },
 })
 

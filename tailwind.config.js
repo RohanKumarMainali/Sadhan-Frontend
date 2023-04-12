487bf52fe3a54e37bb8e9ab4db16d7f795ea6ae1
RohanKumarMainali <rohanmainali39@gmail.com>
2023-04-12 20:48:06 +0545

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-    /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-       "./node_modules/flowbite/**/*.js"
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeOut 5s ease-in-out'
+      },
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeOut: {
+          '0%': { backgroundColor: theme('colors.red.300') },
+          '100%': { backgroundColor: theme('colors.transparent') }
+        }
+      })
+    }
   },
-  plugins: [
-
-
-        require('flowbite/plugin')
-  ],
+  plugins: [require('flowbite/plugin')]
 }
